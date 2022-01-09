fc3b10c99297453791b0ec037ed7d6dc2c859a10
Andy <andy@sentryhealth.ca>
2022-01-09 14:24:09 -0500

    - remove initial tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import Task from "./components/Task";
-
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      <Task task={"asdfa"} />
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
